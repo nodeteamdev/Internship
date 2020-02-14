a51fac536e61382fab4aa67bab8b5e50b1af5065
chechavalerii <chechavalera@gmail.com>
2020-02-14 22:50:38 +0200

    lesson2

--- lesson2/src/config/connection.js
+++ lesson2/src/config/connection.js
@@ -14,6 +14,7 @@ const connectOptions = {
     // flag to allow users to fall back to the old 
     // parser if they find a bug in the new parse
     useNewUrlParser: true,
+    useUnifiedTopology: true
 };
 
 module.exports = mongoose.createConnection(MONGO_URI, connectOptions);
